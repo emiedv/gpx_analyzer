a29d3105c5bb008595d39548e567795904861fcf
emiedv <emiedv@users.noreply.github.com>
2025-01-10 21:42:16 +0100

    Update findSlopes.py

--- findSlopes.py
+++ findSlopes.py
@@ -1,1 +1,4 @@
+# Split arrays into one piece per slope, defined as a continous run with negative elevation change
+input latMetFlt, longmetFlt, elevationFlt, time
+return latMetFlt1, latMetFlt2 etc.
 
